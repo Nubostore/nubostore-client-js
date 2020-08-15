6f67ca108712b997b38d874d78f91d3b8c00ab75
Nubo Developer <developer@nubo.org>
2020-08-15 15:23:34 +0100

    Updated subscription

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import queryString from 'querystring';
-import { ListController, SubscriptionController } from '../controllers';
+import { ListController } from '../controllers';
+import { isBrowser } from '../utils';
 import config from '../config';
 
 export const ACCESS_KEY_HEADER: string = 'x-access-key';
@@ -19,7 +20,7 @@ class NubostoreClient {
   private static instance: NubostoreClient;
   public apiKey: string = '';
   public list: ListController;
-  public subscription: SubscriptionController;
+  private subscriptionRetryAttempts: number = 0;
 
   constructor(options: NubostoreClientOptions) {
     this.apiKey = options.apiKey || '';
@@ -29,7 +30,6 @@ class NubostoreClient {
     }
 
     this.list = new ListController({ client: this });
-    this.subscription = new SubscriptionController({ client: this });
   }
 
   public static getInstance(options: NubostoreClientOptions): NubostoreClient {
@@ -72,6 +72,68 @@ class NubostoreClient {
       return response.data.data;
     } catch (error) {
       throw error.response.data;
+    }
+  }
+
+  async subscription(args: any) {
+    const _this = this;
+    const { regionId, apiKey } = args;
+    const emptyMethod = () => {};
+    const onData = args.onData || emptyMethod;
+    const onError = args.onError || emptyMethod;
+    const onConnect = args.onConnect || emptyMethod;
+    const onDisconnect = args.onDisconnect || emptyMethod;
+    const onFinalDisconnect = args.onFinalDisconnect || emptyMethod;
+    const retryDelay = args.retryDelay || 5000;
+    const maxRetryAttempts = args.maxRetryAttempts || 10;
+    const options = { localeId: args.localeId, records: args.records };
+    const apiKeyId = apiKey || this.apiKey;
+    const optionsString = JSON.stringify(options);
+    const query = {
+      apiKey: apiKeyId,
+      options: optionsString,
+      type: 'records-subscription',
+    };
+    const url = `${
+      config[regionId].CONTENT_SUBSCRIPTION_URL
+    }/subscribe?${queryString.stringify(query)}`;
+
+    const _onConnect = () => {
+      _this.subscriptionRetryAttempts = 0;
+      onConnect();
+    };
+    const _onDisconnect = () => {
+      _this.subscriptionRetryAttempts++;
+      onDisconnect();
+
+      setTimeout(() => {
+        if (_this.subscriptionRetryAttempts < maxRetryAttempts) {
+          _this.subscription(args);
+        } else {
+          onFinalDisconnect();
+        }
+      }, retryDelay);
+    };
+
+    if (isBrowser) {
+      const ws = new WebSocket(url);
+
+      ws.onopen = _onConnect;
+      ws.onerror = onError;
+      ws.onmessage = (event) => {
+        onData(JSON.parse(event.data));
+      };
+      ws.onclose = _onDisconnect;
+    } else {
+      const WebSocket = require('ws');
+      const ws = new WebSocket(url);
+
+      ws.on('open', _onConnect);
+      ws.on('message', (data: any) => {
+        onData(JSON.parse(data));
+      });
+      ws.on('error', onError);
+      ws.on('close', _onDisconnect);
     }
   }
 }
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,1 @@
 export { default as ListController } from './list';
-export { default as SubscriptionController } from './subscription';
